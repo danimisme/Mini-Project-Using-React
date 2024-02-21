3b05d8db212119dde0da06f69c7e9e8d40923568
danimisme <msubhanr53@gmail.com>
2024-02-22 00:39:08 +0700

    merubah header dan menambahkan ke halaman contact

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,13 +1,25 @@
 import "animate.css/animate.css";
 
-const Header = ({ title, subTitle, imageUrl, caption }) => {
+const Header = ({ title, subTitle, imageUrl, caption, variant = "" }) => {
   return (
     <header>
       <div className="container-fluid my-5 header-container">
         <div className="container">
-          <div className="row d-flex align-items-center flex-column-reverse flex-md-row">
-            <div className="col-md-6 text-end  ">
-              <h1 className="fw-bold title_header animate__animated animate__fadeInLeft">
+          <div
+            className={`row d-flex align-items-center flex-column-reverse flex-md-row ${variant} `}
+          >
+            <div
+              className={`col-md-6 ${
+                variant === "header-right" ? "text-start" : "text-end"
+              }`}
+            >
+              <h1
+                className={`fw-bold title_header animate__animated  ${
+                  variant === "header-right"
+                    ? "animate__fadeInRight"
+                    : "animate__fadeInLeft"
+                }`}
+              >
                 {title} {subTitle && <div>{subTitle}</div>}
               </h1>
               <blockquote className="fs-3 caption_header animate__animated animate__fadeInUp">
@@ -17,8 +29,12 @@ const Header = ({ title, subTitle, imageUrl, caption }) => {
             <div className="col-md-4">
               <img
                 src={imageUrl}
-                alt="diminum caffe"
-                className="img-fluid mx-3 image_header animate__animated animate__fadeInRight"
+                alt="image header"
+                className={`img-fluid mx-3 image_header animate__animated ${
+                  variant === "header-right"
+                    ? "animate__fadeInLeft"
+                    : "animate__fadeInRight"
+                }`}
               />
             </div>
           </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,18 @@
 import Modals from "../components/Elements/Modals";
 import Navbar from "../components/Fragments/Navbar";
+import Header from "../components/Fragments/Header";
 
 const Contact = () => {
   return (
     <>
       <Navbar />
+      <Header
+        variant="header-right"
+        title="Kepuasan Anda Adalah Segalanya!"
+        caption="Kami mengutamakan kenyamanan pelanggan. Rasakan pengalaman kopi
+      yang menyenangkan."
+        imageUrl="images/barista-vector.jpg"
+      />
       <Modals />
     </>
   );
